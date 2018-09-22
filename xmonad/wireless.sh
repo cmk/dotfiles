5a4b006e1cc9cc0918ad78e8bbf5e07a1bf61559
Chris McKinlay <chris.mckinlay@gmail.com>
2018-09-22 14:31:55 -0700

    Update wireless.sh

--- xmonad/wireless.sh
+++ xmonad/wireless.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-essid=`nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2`
-strength=`nmcli -f ACTIVE,SIGNAL dev wifi list | awk '$1=="yes" {print $2}'`
+essid=`nmcli -t -f active,ssid   dev wifi | egrep '^yes' | cut -d':' -f2`
+strength=`nmcli -f active,signal dev wifi | awk '$1=="yes" {print $2}'`
 echo $essid $strength
 
 exit 0
